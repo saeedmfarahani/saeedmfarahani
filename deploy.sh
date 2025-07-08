83c6ad0eaf2bcff640381b9a1345ab319aefd43a
SaeedMFarahani <silitonix@gmail.com>
2025-07-08 11:01:26 +0330

    :rocket: deploy

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
-mkdir docs
 bun run build &&
-rm -r docs/* &&
-cp CNAME docs/CNAME &&
-cp .nojekyll docs/.nojekyll
-mv build/client/* docs &&
-cp ./docs/index.html ./docs/404.html
+mv ./build/client ./docs &&
+cp ./CNAME ./docs/ &&
+cp ./.nojekyll ./docs/ &&
+cp ./docs/index.html ./docs/404.html &&
 rm -r build &&
 git add . &&
 git commit -m ":rocket: deploy" &&
